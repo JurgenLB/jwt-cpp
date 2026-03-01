b7c106afefaad453c1d086bd134a9a4792156826
JurgenLB <77586573+JurgenLB@users.noreply.github.com>
2026-03-01 14:26:36 +0100

    update jwt.h

--- include/jwt-cpp/jwt.h
+++ include/jwt-cpp/jwt.h
@@ -65,7 +65,9 @@
 #endif
 
 #if defined(LIBRESSL_VERSION_NUMBER)
-#if LIBRESSL_VERSION_NUMBER >= 0x3050300fL
+#if LIBRESSL_VERSION_NUMBER >= 0x3070100fL // 3.7.1 - EdDSA support
+#define JWT_OPENSSL_1_1_1
+#elif LIBRESSL_VERSION_NUMBER >= 0x3050300fL // 3.5.3
 #define JWT_OPENSSL_1_1_0
 #else
 #define JWT_OPENSSL_1_0_0
@@ -1409,7 +1411,7 @@ namespace jwt {
 				std::string res(static_cast<size_t>(EVP_MAX_MD_SIZE), '\0');
 				auto len = static_cast<unsigned int>(res.size());
 				if (HMAC(md(), secret.data(), static_cast<int>(secret.size()),
-						 reinterpret_cast<const unsigned char*>(data.data()), static_cast<int>(data.size()),
+						 reinterpret_cast<const unsigned char*>(data.data()), data.size(),
 						 (unsigned char*)res.data(), // NOLINT(google-readability-casting) requires `const_cast`
 						 &len) == nullptr) {
 					ec = error::signature_generation_error::hmac_failed;
@@ -1814,6 +1816,7 @@ namespace jwt {
 		 *
 		 * The EdDSA algorithms were introduced in [OpenSSL v1.1.1](https://www.openssl.org/news/openssl-1.1.1-notes.html),
 		 * so these algorithms are only available when building against this version or higher.
+		 * LibreSSL added EdDSA (Ed25519) functionality in [LibreSSL 3.7.1](https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-3.7.1-relnotes.txt)
 		 */
 		struct eddsa {
 			/**
@@ -1857,10 +1860,9 @@ namespace jwt {
 				size_t len = EVP_PKEY_size(pkey.get());
 				std::string res(len, '\0');
 
-// LibreSSL is the special kid in the block, as it does not support EVP_DigestSign.
-// OpenSSL on the otherhand does not support using EVP_DigestSignUpdate for eddsa, which is why we end up with this
-// mess.
-#if defined(LIBRESSL_VERSION_NUMBER) || defined(LIBWOLFSSL_VERSION_HEX)
+// LibreSSL and OpenSSL, require the oneshot EVP_DigestSign API.
+// wolfSSL uses the Update/Final pattern.
+#if defined(LIBWOLFSSL_VERSION_HEX)
 				ERR_clear_error();
 				if (EVP_DigestSignUpdate(ctx.get(), reinterpret_cast<const unsigned char*>(data.data()), data.size()) !=
 					1) {
@@ -1901,10 +1903,9 @@ namespace jwt {
 					ec = error::signature_verification_error::verifyinit_failed;
 					return;
 				}
-// LibreSSL is the special kid in the block, as it does not support EVP_DigestVerify.
-// OpenSSL on the otherhand does not support using EVP_DigestVerifyUpdate for eddsa, which is why we end up with this
-// mess.
-#if defined(LIBRESSL_VERSION_NUMBER) || defined(LIBWOLFSSL_VERSION_HEX)
+// LibreSSL and OpenSSL, require the oneshot EVP_DigestVerify API.
+// wolfSSL uses the Update/Final pattern.
+#if defined(LIBWOLFSSL_VERSION_HEX)
 				if (EVP_DigestVerifyUpdate(ctx.get(), reinterpret_cast<const unsigned char*>(data.data()),
 										   data.size()) != 1) {
 					ec = error::signature_verification_error::verifyupdate_failed;
@@ -1915,7 +1916,7 @@ namespace jwt {
 					ec = error::signature_verification_error::verifyfinal_failed;
 					return;
 				}
-#else
+#else // !LIBWOLFSSL_VERSION_HEX (OpenSSL/LibreSSL)
 				auto res = EVP_DigestVerify(ctx.get(), reinterpret_cast<const unsigned char*>(signature.data()),
 											signature.size(), reinterpret_cast<const unsigned char*>(data.data()),
 											data.size());
@@ -2218,7 +2219,7 @@ namespace jwt {
 		 *
 		 * https://en.wikipedia.org/wiki/EdDSA#Ed25519
 		 *
-		 * Requires at least OpenSSL 1.1.1.
+		 * Requires at least OpenSSL 1.1.1. or LibreSSL 3.7.1.
 		 */
 		struct ed25519 : public eddsa {
 			/**
@@ -2235,12 +2236,13 @@ namespace jwt {
 				: eddsa(public_key, private_key, public_key_password, private_key_password, "EdDSA") {}
 		};
 
+#if !defined(LIBRESSL_VERSION_NUMBER)
 		/**
 		 * Ed448 algorithm
 		 *
 		 * https://en.wikipedia.org/wiki/EdDSA#Ed448
 		 *
-		 * Requires at least OpenSSL 1.1.1.
+		 * Requires at least OpenSSL 1.1.1. Note: Not supported by LibreSSL.
 		 */
 		struct ed448 : public eddsa {
 			/**
@@ -2256,7 +2258,8 @@ namespace jwt {
 						   const std::string& public_key_password = "", const std::string& private_key_password = "")
 				: eddsa(public_key, private_key, public_key_password, private_key_password, "EdDSA") {}
 		};
-#endif
+#endif // !LIBRESSL_VERSION_NUMBER
+#endif // !JWT_OPENSSL_1_0_0 && !JWT_OPENSSL_1_1_0
 
 		/**
 		 * PS256 algorithm
